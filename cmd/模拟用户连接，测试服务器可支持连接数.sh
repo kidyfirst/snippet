b1a6687023cd4b33316c293e5cb0f953423e6013
timlv <kidyfirst@126.com>
2016-04-15 14:42:39 +0800

    增加新demo

--- cmd/模拟用户连接，测试服务器可支持连接数.sh
+++ cmd/模拟用户连接，测试服务器可支持连接数.sh
@@ -3,13 +3,26 @@
 if [ ! -n "$1" ] ;then
     echo "you have not input a number!"
 else
-    c=$1
-    while [ $c -gt 0 ]
-    do
-        c=$(($c-1))
-        {
-        curl -s --connect-timeout 10 http://10.153.130.84:8081/jsonp?test=1\&cb=jQuery19101696983754077237_1460434\&uin=383466934\&skey=txqyzfi > /dev/null 2>&1
-         }&
-        sleep 0.05
-    done
+        url=http://10.153.130.84:8081/jsonp?test=1\&cb=jQuery19101696983754077237_1460434\&uin=383466934\&skey=txqyzfi
+        one=100
+        c=$(($1/100))
+        count=$c
+        t=$1
+        start=$(date +%s)
+        while [ $c -gt 0 ]
+        do
+                c=$(($c-1))
+                {
+                        /usr/local/apache/bin/ab  -k -n $one -c $one $url > /dev/null 2>&1
+                }&
+                echo $(($t-($c*$one))) >> log_connection_send.log
+                sleep 0.5
+        done
+        wait
+        end=$(date +%s)
+        echo "total send $(($count*$one)) request, spend time $(($end-$start))(s)!"
 fi
+
+
+
+#./fileName -100  ##发送url 100次
